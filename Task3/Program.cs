653cdb9ca00529f792e9368fc70f552ec68e8bf4
ownermac <mcls@list.ru>
2023-02-15 20:57:49 +0300

    Finish

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task3");
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task4");
+            Console.ReadKey();
+        }
+    }
+}
+
